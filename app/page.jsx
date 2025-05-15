3f5fa3dfd362a2582a5479c3699a2c3526880590
paul <paul1.david@epitech.eu>
2025-05-15 22:02:25 +0200

    [fix]select

--- app/page.jsx
+++ app/page.jsx
@@ -12,9 +12,9 @@ export default function Home() {
     <div className="">
       <Bus />
       <Form/>
-      <div className="fond">
-        <Image className='perso2 mix-blend-hard-light opacity-40 -z-1 w-[60vw]  sm:w-[60vw] md:w-[40vw] xl:w-[30vw] absolute -right-20 -bottom-5' src='/assets/perso2.png' width={2000} height={1000} alt='bus'/>
-        <Image className='perso1 mix-blend-hard-light opacity-40 -z-1 w-[60vw]  sm:w-[60vw] md:w-[40vw] xl:w-[30vw] absolute -left-0 -bottom-5' src='/assets/perso1.png' width={2000} height={1000} alt='bus'/>
+      <div className="fond relative">
+        <Image className='perso2 mix-blend-hard-light opacity-40 z-[-1] w-[60vw]  sm:w-[60vw] md:w-[40vw] xl:w-[30vw] absolute -right-20 -bottom-30' src='/assets/perso2.png' width={2000} height={1000} alt='bus'/>
+        <Image className='perso1 mix-blend-hard-light opacity-40 z-[-1] w-[60vw]  sm:w-[60vw] md:w-[40vw] xl:w-[30vw] absolute -left-0   -bottom-30' src='/assets/perso1.png' width={2000} height={1000} alt='bus'/>
       </div>
     </div>
   );
